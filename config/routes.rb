7e8227a0a333ad7625233bcc061c773e457039ae
Adamboots <adamplishka@hotmail.com>
2020-11-01 15:57:13 -0600

    Completed Part 2 with fixes on previous parts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "customers/index", to: "customers#index"
+  get "customers/alphabetized", to: "customers#alphabetized"
+  get "customers/missing-email", to: "customers#missing_email"
 end
--- db/migrate/20201030202023_create_customers.rb
+++ db/migrate/20201030202023_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
       t.string :full_name
-      t.integer :phone_number
+      t.string :phone_number
       t.string :email
       t.text :notes
 
