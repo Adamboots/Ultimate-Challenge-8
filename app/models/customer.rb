648b3f7c15110a45801ef3c782fbc150f432ed6b
Adamboots <adamplishka@hotmail.com>
2020-11-01 15:00:48 -0600

    Fix for setup to allow missing email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  validates :full_name, :email, presence: true, length: { minimum: 1 }
+  validates :full_name, presence: true, length: { minimum: 1 }
   has_one_attached :image
 end
